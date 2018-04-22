ce9b353c784e0097cb1e613ebfbd3adc6bdc085f
Daire Connolly <35735716+DaireConnolly@users.noreply.github.com>
2018-04-22 22:55:02 +0100

    Loads Image File

--- camanImageEditor.js
+++ camanImageEditor.js
@@ -1,1 +1,12 @@
 // JavaScript source code
+document.addEventListener("DOMContentLoaded", function(){
+	//Loads and replaces loaded image
+	var loadImage = document.getElementById("load");
+	function loadInputHandler(event) {
+		var imageFile = event.target.files[0];
+		var imageElement = document.getElementById("image");
+		imageElement.setAttribute("src", URL.createObjectURL(imageFile));
+		//document.getElementById("image").width = "50%";
+	};
+	loadImage.onchange = loadInputHandler;
+}, false);
